f2ec379c153c3937379780a6dcc0fcdf761b424e
MrTalon63 <marcin@mrtalon.eu>
2026-07-25 18:39:24 +0200

    When it rains it pours I guess

--- windows/Configure-vcpkg.ps1
+++ windows/Configure-vcpkg.ps1
@@ -87,7 +87,7 @@ vcpkg_cmake_configure(
 (Get-Content -raw .\ports\lapack-reference\portfile.cmake) -replace 'vcpkg_cmake_configure\s*\(', $lapack_patch | Set-Content -Encoding ASCII .\ports\lapack-reference\portfile.cmake
 
 # Core packages. libxml2 is for libiio
-.\vcpkg install --triplet $platform pthreads libjpeg-turbo tiff libpng glfw3 libusb fftw3 libxml2 portaudio nng zstd armadillo opencl curl[schannel] hdf5[cpp] sqlite3
+.\vcpkg install --triplet $platform pthreads libjpeg-turbo tiff libpng glfw3 libusb fftw3 libxml2 portaudio nng zstd opencl curl[schannel] hdf5[cpp] sqlite3
 
 # Entirely for UHD...
 .\vcpkg install --triplet $platform boost-chrono boost-date-time boost-filesystem boost-program-options boost-system boost-serialization boost-thread `
